40839b5e7ae8351c271a8917d11ba4a051c18a7c
Abdulkarim El Khazaani <abdulkarim.elkhazaani@octoenergy.com>
2018-10-26 23:41:11 +0100

    Format post dates

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { format } from 'date-fns';
+import './PostListing.css';
+
+const formatDate = (date) => format(date, 'D MMM, YYYY');
 
 class PostListing extends React.Component {
     getPostList() {
@@ -24,7 +28,9 @@ class PostListing extends React.Component {
                 {/* Your post list here. */
                     postList.map(post => (
                         <li key={post.title}>
-                            <span className='post-meta'>{post.date}</span>
+                            <span className='post-meta'>
+                                {formatDate(post.date)}
+                            </span>
                             <Link
                                 className='post-link'
                                 to={post.path}
