b482eb99b3ac7f6e27227b87567c236b3d965806
Abdulkarim El Khazaani <abdulkarim.elkhazaani@octoenergy.com>
2018-10-28 16:24:56 +0000

    Add wireframe sphere animation

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
+import './Canvas.css';
 
 class Canvas extends Component {
     constructor(props) {
@@ -15,24 +16,25 @@ class Canvas extends Component {
         const height = this.mount.clientHeight
 
         const scene = new THREE.Scene()
-        scene.background = new THREE.Color(0xffffff)
+        // scene.background = new THREE.Color(0xffffff)
         const camera = new THREE.PerspectiveCamera(
-            75,
+            70,
             width / height,
             0.1,
             1000
         )
         const renderer = new THREE.WebGLRenderer({ antialias: true })
-        const geometry = new THREE.SphereGeometry(1, 1, 1)
+        const geometry = new THREE.SphereGeometry(1, 10, 5)
         const material = new THREE.MeshBasicMaterial({
-            color: 0xff0000,
+            color: 0xFF4040,
             wireframe: true,
         })
         const cube = new THREE.Mesh(geometry, material)
 
-        camera.position.z = 4
+        camera.position.y = 0.2
+        camera.position.z = 2.4
         scene.add(cube)
-        renderer.setClearColor('#ffffff')
+        renderer.setClearColor('#ffffff');
         renderer.setSize(width, height)
 
         this.scene = scene
@@ -75,7 +77,7 @@ class Canvas extends Component {
     render() {
         return (
             <div
-                style={{ width: '400px', height: '400px' }}
+                className='canvas-wrapper'
                 ref={(mount) => { this.mount = mount }}
             />
         )
