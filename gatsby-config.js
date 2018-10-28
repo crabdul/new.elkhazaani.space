9a6bdc8aee67cfc5c605ed0d4fd1b605fad4844e
Abdulkarim El Khazaani <abdulkarim.elkhazaani@octoenergy.com>
2018-10-28 16:36:07 +0000

    Update site config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,17 +2,15 @@ const config = require('./data/SiteConfig');
 const urljoin = require('url-join');
 
 module.exports = {
-    pathPrefix: config.pathPrefix,
     siteMetadata: {
-        siteUrl: urljoin(config.siteUrl, config.pathPrefix),
+        siteUrl: urljoin(config.siteUrl),
         rssMetadata: {
-            site_url: urljoin(config.siteUrl, config.pathPrefix),
-            feed_url: urljoin(config.siteUrl, config.pathPrefix, config.siteRss),
+            site_url: urljoin(config.siteUrl),
+            feed_url: urljoin(config.siteUrl, config.siteRss),
             title: config.siteTitle,
             description: config.siteDescription,
             image_url: `${urljoin(
                 config.siteUrl,
-                config.pathPrefix
             )}/logos/logo-512.png`,
             author: config.userName,
             copyright: config.copyright
@@ -76,7 +74,6 @@ module.exports = {
                 name: config.siteTitle,
                 short_name: config.siteTitleShort,
                 description: config.siteDescription,
-                start_url: config.pathPrefix,
                 background_color: config.backgroundColor,
                 theme_color: config.themeColor,
                 display: 'minimal-ui',
