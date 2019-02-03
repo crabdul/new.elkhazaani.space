0d76c51995db0f884e7d96f2ff01a832aca67273
crabdul <23074754+crabdul@users.noreply.github.com>
2019-02-03 11:05:45 +0000

    Update site config
    > Add GA
    > Update path prefix
    > Update copyright
    > Update site description

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,16 +4,16 @@ module.exports = {
     siteTitleAlt: 'El Khazaani', // Alternative site title for SEO.
     siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
     siteUrl: 'https://elkhazaani.space', // Domain of your website without pathPrefix.
-    pathPrefix: '/gatsby-advanced-starter', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-    siteDescription: 'A GatsbyJS stater with Advanced design in mind.', // Website description used for RSS feeds/meta description tag.
+    pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+    siteDescription: "my thoughts on programming related things", // Website description used for RSS feeds/meta description tag.
     siteRss: '/rss.xml', // Path to the RSS file.
     siteFBAppID: '', // FB Application ID for using app insights
-    googleAnalyticsID: '', // GA tracking ID.
+    googleAnalyticsID: 'UA-116304579-1', // GA tracking ID.
     postDefaultCategoryID: 'Tech', // Default category for posts.
     userName: 'Karim El Khazaani',
     dateFromFormat: 'YYYY-MM-DD', // Date format used in the frontmatter.
     dateFormat: 'YYYY-MM-DD', // Date format for display.
-    copyright: 'Copyright © 2018. Karim El Khazaani', // Copyright string for the footer of the website and RSS feed.
+    copyright: 'Copyright © 2019. Karim El Khazaani', // Copyright string for the footer of the website and RSS feed.
     themeColor: '#c62828', // Used for setting manifest and progress theme colors.
     backgroundColor: '#e0e0e0' // Used for setting manifest background color.
 };
